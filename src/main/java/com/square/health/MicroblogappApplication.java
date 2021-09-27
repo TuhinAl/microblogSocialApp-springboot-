5705fddabe0c09f9616889e2fd3439a5154b4fba
tuhinal <alauddintuhin1411@gmail.com>
2021-09-27 23:00:40 +0600

    initial commit

---
+++ src/main/java/com/square/health/MicroblogappApplication.java
@@ -0,0 +1,13 @@
+package com.square.health;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroblogappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroblogappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/square/health/MicroblogappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.square.health;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroblogappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
